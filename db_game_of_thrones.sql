bd8287c618954d5ca8bd3b77a818d71df517c377
leovillar94 <114249176+leovillar94@users.noreply.github.com>
2023-02-03 14:25:43 -0300

    Update db_game_of_thrones.sql

--- db_game_of_thrones.sql
+++ db_game_of_thrones.sql
@@ -10,16 +10,16 @@ CREATE TABLE GOT_episodes (
     PRIMARY KEY (id)
 );
 
-/*Qual é o episódio mais longo ?*/
+/*Qual o nome do único dragão de Daenerys que ficou vivo ?*/
 SELECT COUNT(DISTINCT season) AS quantidade_de_episódios FROM got_episodes;
 
-/*Qual é o pior episódio?*/
+/*Quantas horas possui a temporada 8 de game of Thornes?*/
 SELECT COUNT(episode) AS avaliações_de_episódios FROM got_episodes;
 
-/*Quantos episódios cada temporada possui?*/
+/*Qual temporada e mais bem avaliada pelo público*/
 SELECT season, COUNT(episode) AS quantidade_de_episódios_por_temporada FROM got_episodes GROUP BY season;
 
-/*Qual o episódio mais bem avaliado de cada temporada?*/
+/*Qual temporada e mais bem avaliada pelo público**/
 SELECT * FROM got_episodes WHERE  season = 1  ORDER BY rating DESC LIMIT 1;
 SELECT * FROM got_episodes WHERE  season = 2  ORDER BY rating DESC LIMIT 1;
 SELECT * FROM got_episodes WHERE  season = 3  ORDER BY rating DESC LIMIT 1;
@@ -29,8 +29,6 @@ SELECT * FROM got_episodes WHERE  season = 6 ORDER BY rating DESC LIMIT 1;
 SELECT * FROM got_episodes WHERE  season = 7 ORDER BY rating DESC LIMIT 1;
 SELECT * FROM got_episodes WHERE  season = 8 ORDER BY rating DESC LIMIT 1;
 
-/*Qual o episódio com maior duração?*/
+/*Qual episódio ganhou destaque na série?*/
 SELECT season, episode, title, duration FROM got_episodes ORDER BY duration DESC;
 
-/*Qual o episódio com menor duração?*/ 
-SELECT season, episode, title, duration FROM got_episodes ORDER BY duration ASC;
