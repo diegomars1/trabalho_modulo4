b9596995d34c425eb32b78a44552f0cf2af0dd75
Diego Bezerra <67666130+diegomars1@users.noreply.github.com>
2023-02-05 21:29:56 -0300

    Update db_game_of_thrones.sql
    atualização do codigo e foram inseridas mais duas perguntas.

--- db_game_of_thrones.sql
+++ db_game_of_thrones.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE db_game_of_thrones;
+
 USE db_game_of_thrones;
 CREATE TABLE GOT_episodes (
     id INT NOT NULL AUTO_INCREMENT,
@@ -10,19 +10,30 @@ CREATE TABLE GOT_episodes (
     PRIMARY KEY (id)
 );
 
-/*Quantas horas possui a temporada 8 de game of Thornes?*/
-SELECT COUNT(DISTINCT season) AS Ao todo, todas as oito temporadas de Game Of Thrones levariam 70 horas e 14 minutos para assistir .FROM got_episodes;
+/*Quantos minutos no total tem a série? */
+SELECT SUM(duration) AS total from GOT_episodes;
 
-/*Quantos episódios a série possuí?*/
-SELECT COUNT(episode) AS a serie possui um total de 73 episodios FROM got_episodes;
+/*Quantas temporadas a série possui?*/
+SELECT COUNT(DISTINCT season) AS quantidade_de_temporadas FROM got_episodes;
 
-/*Qual temporada e mais bem avaliada pelo público?*/
-SELECT season, COUNT(episode) AS Quase 20 milhões de pessoas acompanharam o episódio final da série "Game of thrones" nos Estados Unidos, um recorde para a HBO, que divulgou os números da audiência nesta segunda-feira. FROM got_episodes GROUP BY season;
+/*Quantos episódios a série possui?*/
+SELECT COUNT(episode) AS quantidade_de_episódios FROM got_episodes;
 
-/*Qual episódio ganhou destaque na série?*/
-SELECT COUNT(episode) a 4ª temporada firmou a série da HBO como um dos melhores programas de TV. FROM got_episodes;
+/*Quantos episódios cada temporada possui?*/
+SELECT season, COUNT(episode) AS quantidade_de_episódios_por_temporada FROM got_episodes GROUP BY season;
 
+/* Qual o episódio com menor avaliação da série?*/
+SELECT MIN(rating) FROM GOT_episodes;
 
-/*Qual é o tempo total da série?*/
-SELECT Em 2019, a HBO informou que a última temporada teve em média 44,2 milhões de espectadores por episódio em audiência bruta: mais de 10 milhões de espectadores em relação à temporada anterior. FROM got_episodes ORDER BY duration DESC;
+/*Qual o episódio mais bem avaliado de cada temporada?*/
+SELECT * FROM got_episodes WHERE  season = 1  ORDER BY rating DESC LIMIT 1;
+SELECT * FROM got_episodes WHERE  season = 2  ORDER BY rating DESC LIMIT 1;
+SELECT * FROM got_episodes WHERE  season = 3  ORDER BY rating DESC LIMIT 1;
+SELECT * FROM got_episodes WHERE  season = 4  ORDER BY rating DESC LIMIT 1;
+SELECT * FROM got_episodes WHERE  season = 5 ORDER BY rating DESC LIMIT 1;
+SELECT * FROM got_episodes WHERE  season = 6 ORDER BY rating DESC LIMIT 1;
+SELECT * FROM got_episodes WHERE  season = 7 ORDER BY rating DESC LIMIT 1;
+SELECT * FROM got_episodes WHERE  season = 8 ORDER BY rating DESC LIMIT 1;
 
+/*Qual episódio com maior duração ?*/
+SELECT MAX(duration) FROM GOT_episodes;
